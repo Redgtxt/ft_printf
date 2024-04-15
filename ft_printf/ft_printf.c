6ee1ea487da6add92dd61bb98556d230c9bcfd3c
Hugo Miguel Guerreiro Pinto <hguerrei@c1r9s1.42lisboa.com>
2024-04-15 13:03:35 +0100

    podes escrever

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -28,6 +28,25 @@ int	ft_conversions(const char *format, ...)
 	return (0);
 }
 
+ int ft_printf(const char *str, ...)
+ {
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if(str[i] == '%')
+		{
+			//Passa Script das conversoes
+		}else{
+			write(1,&str[i],1);
+		}
+		i++;
+	}
+
+	return(0);
+ }
+
 int	main(void)
 {
 	char	c;
@@ -39,5 +58,6 @@ int	main(void)
 	c = 'v';
 	f = 'f';
 	ft_conversions(" %c %c %s %d %i\n", c, f, nome,num,numOct);
-	printf("\nOriginal: %c %c %s %d %i ", c, f, nome,num,numOct);
+	printf("\nOriginal: %c %c %s %d %i \n", c, f, nome,num,numOct);
+	ft_printf("Bom dia darling 45353");
 }
